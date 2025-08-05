6a27ed89e2ac74b08865ba4eb6133d2fcdcf179b
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-08-06 02:38:18 +0300

    Exercise 24 : Draw A Game Board
    This is a part of the Practice Python series.

---
+++ #1 from practicepython.org/#24 Draw A Game Board/main.py
@@ -0,0 +1,21 @@
+# Draw A Game Board
+
+user_c = int(input("Enter the Size (3x3) is 3   (8x8) is 8   (19x19) is 19: "))
+
+
+# Draw the game board
+def horizontal_line():
+    print("+---" * user_c + "+") 
+
+def vertical_line():
+    print("|   " * (user_c+1) )
+
+def draw_board():
+    for i in range(user_c):
+        horizontal_line()
+        vertical_line()
+    horizontal_line()
+
+# Call the function to draw the board
+draw_board()
+print("Game board drawn successfully!")
