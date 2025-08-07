37a766a5b7b20cf74048dfd4010eccd0eefd1e02
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-08-07 04:07:23 +0300

    Exercise 38 : f Strings It is same as Exercise 1 becasue i did it this way

---
+++ #1 from practicepython.org/#38 f Strings/main.py
@@ -0,0 +1,8 @@
+
+#It is the same one as Exercise 1 becasue i solved it this way .
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+
+y100 = 2024 - age + 100
+
+print(f"Hello, {name}! You are {age} years old. You will be 100 years old in the year {y100}.")
